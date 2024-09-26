9e1ee68b79816a99dd418239ab94e515ed3482db
Alonso Sánche Moreno de Vega <asanmor0410@cpifpalanturing.es>
2024-09-26 14:43:51 +0200

    Avances

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // src/index.ts
-import {Observable, Subscriber} from 'rxjs';
+
 document.addEventListener('DOMContentLoaded', () => {
     const generarBoton = document.getElementById('generateButton') as HTMLButtonElement;
     const nombreInput = document.getElementById('nameInput') as HTMLInputElement;
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
     generarBoton.addEventListener('click', () =>{
         const nombre  = nombreInput.value as string;
 
+        
         mensajeError.style.display='none';
         new Promise((resolve, reject) =>{
             setTimeout(() =>{
@@ -39,47 +40,4 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    const imageButton = document.getElementById('generateImage') as HTMLButtonElement;
-    const imgContainer = document.getElementById('imgContainer')!;
-
-    const mostrarImagen$ = new Observable((subscriber) => {
-        const click = () => {
-            subscriber.next();
-        };
-
-        imageButton.addEventListener('click', click);
-
-        return () => {
-            imageButton.removeEventListener('click', click);
-        };
-    });
-
-    mostrarImagen$.subscribe(() => {
-        fetch('https://api.thecatapi.com/v1/images/search') // Usar fetch para obtener la imagen
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Error en la red');
-                }
-                return response.json(); // Parsear la respuesta a JSON
-            })
-            .then(data => {
-                const imgUrl = data[0].url; // Asumimos que la respuesta es un array
-
-                const img = document.createElement('img');
-                img.src = imgUrl; // Usar la URL de la imagen obtenida
-                img.alt = 'Imagen de ejemplo';
-                img.style.display = 'block';
-                img.style.marginTop = '10px';
-
-                // Añadir la nueva imagen al contenedor
-                imgContainer.appendChild(img);
-            })
-            .catch(error => {
-                console.error('Error al obtener la imagen:', error);
-                const errorImg = document.createElement('p');
-                errorImg.textContent = 'Error al cargar la imagen';
-                errorImg.style.color = 'red';
-                imgContainer.appendChild(errorImg);
-            });
-    });
 });
